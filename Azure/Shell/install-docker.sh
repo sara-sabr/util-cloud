777b5a1967a97f4bc2381433e0e32195ad74e14e
Eric Wu <e-wu@users.noreply.github.com>
2020-03-03 08:34:21 -0500

    Install docker

---
+++ Azure/Shell/install-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+echo "Relogin to run docker commands as current user"

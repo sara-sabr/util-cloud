1885a354d556d3c19c99be1dc048c2d25d02a012
Wu <2658955+e-wu@users.noreply.github.com>
2020-03-05 08:54:39 -0500

    Add in scripts and update

--- Azure/Shell/install-docker.sh
+++ Azure/Shell/install-docker.sh
@@ -8,4 +8,7 @@ apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "Relogin to run docker commands as current user"
